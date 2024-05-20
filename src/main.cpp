af9fc73af090eff1beff453318e293acaa74d5db
GiovanePS <giovanespace@gmail.com>
2024-05-20 11:34:44 -0300

    feat: uso básico de threads com pthreads

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include "headers/Player.hpp"
+#include <pthread.h>
+
+struct ThreadData {
+  int id;
+  sf::RenderWindow *window;
+};
+
+void *thread_printing(void *data);
 
 int main() {
-  // Window configurations
+  // Window settings
   sf::VideoMode screensize = sf::VideoMode::getDesktopMode();
   sf::RenderWindow window(screensize, "Space Defenders");
 
+
+  // Texture settings
   sf::Texture playerTexture;
   if (!playerTexture.loadFromFile("assets/spaceship.png")) {
     return -1;
   }
   Player player(&playerTexture);
+
+  // Thread settings
+  pthread_t threads[1];
+  ThreadData thread;
+  thread.id = 1;
+  thread.window = &window;
+  pthread_create(&threads[0], NULL, thread_printing, (void*)&thread);
 
   while (window.isOpen()) {
     sf::Event event;
@@ -31,5 +49,19 @@ int main() {
     window.display();
   }
 
+  pthread_join(threads[0], NULL);
+
   return 0;
+}
+
+void *thread_printing(void *arg) {
+  ThreadData *data = (ThreadData*)(arg);
+  sf::RenderWindow *window = data->window;
+
+  while (window->isOpen()) {
+  std::cout << "Thread is printing!!" << std::endl;
+  }
+  std::cout << "Bye!!" << std::endl;
+
+  pthread_exit(NULL);
 }
